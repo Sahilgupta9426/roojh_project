6daecba6818f7957ed3393faf0fafbd929a83841
Sahil Gupta <sahil.sg39@gmail.com>
2022-05-07 21:52:55 +0530

    double tap to exit

--- lib/homepage/home.dart
+++ lib/homepage/home.dart
@@ -37,43 +37,64 @@ class _HomeState extends State<Home> {
   //   });
   // }
   //2nd part
-
+  DateTime? lastpresed;
   @override
   Widget build(BuildContext context) {
     userSignedIn = useProvider(UserLoggedInProvider);
     return Scaffold(
-      body: Column(
-        children: [
-          TopImagesField(),
-          SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Column(
-              children: [
-                Text(
-                  'Hello User ',
-                  style: TextStyle(fontSize: 30),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  height: 40,
-                  width: MediaQuery.of(context).size.width,
-                  child: ElevatedButton(
-                      onPressed: () {
-                        // AuthController.logOut();
-                        AuthServices().signOut(context, userSignedIn);
-                        Navigator.pushReplacementNamed(context, "/login");
-                        // Navigator.of(context).push(
-                        //     MaterialPageRoute(builder: (context) => SignIn()));
-                      },
-                      child: Text('logout')),
-                )
-              ],
+      body: WillPopScope(
+        onWillPop: () async {
+          final now = DateTime.now();
+          final maxDuration = Duration(seconds: 2);
+          final isWarning =
+              lastpresed == null || now.difference(lastpresed!) > maxDuration;
+          if (isWarning) {
+            lastpresed = DateTime.now();
+            final snackBar = SnackBar(
+              content: Text('Double Tap To Close'),
+              duration: maxDuration,
+            );
+            ScaffoldMessenger.of(context)
+              ..removeCurrentSnackBar()
+              ..showSnackBar(snackBar);
+            return false;
+          } else {
+            return true;
+          }
+        },
+        child: Column(
+          children: [
+            TopImagesField(),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Hello User ',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 40,
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          // AuthController.logOut();
+                          AuthServices().signOut(context, userSignedIn);
+                          Navigator.pushReplacementNamed(context, "/login");
+                          // Navigator.of(context).push(
+                          //     MaterialPageRoute(builder: (context) => SignIn()));
+                        },
+                        child: Text('logout')),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
